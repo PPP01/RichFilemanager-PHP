3bdf4229970801ed31d42492eb719cc15b774f45
Patric Pesch <p.pesch@11com7.de>
2023-06-17 18:41:04 +0200

    Component\EventDispatcher\Event -> Contracts\EventDispatcher\Event

--- src/Event/Api/AfterFileExtractEvent.php
+++ src/Event/Api/AfterFileExtractEvent.php
@@ -2,8 +2,8 @@
 
 namespace RFM\Event\Api;
 
-use Symfony\Component\EventDispatcher\Event;
 use RFM\Repository\ItemData;
+use Symfony\Contracts\EventDispatcher\Event;
 
 /**
  * API event. Dispatched each time an archive has been extracted.
